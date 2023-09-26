49d64a88a177fb96e76fcdd8acef2b68d3eefac1
Lili Montano <65881308+lilibella28@users.noreply.github.com>
2023-09-26 16:44:40 +0000

    Challenge Array

--- src/Ch01-Arrays/01_02/end/sum-array.cpp
+++ src/Ch01-Arrays/01_02/end/sum-array.cpp
@@ -4,6 +4,20 @@ using namespace std;
 int calculateSum(const int arr[], size_t size)
 {
     //...TODO: implement
+    int sumElement = 0;
+    if (size == 0)
+    {
+        return 0;
+    }
+    if (size == 1)
+    {
+        return arr[0];
+    }
+    for (size_t i = 0; i < size; i++)
+    {
+        sumElement += arr[i];
+    }
+    return sumElement;
 }
 
 int main()
